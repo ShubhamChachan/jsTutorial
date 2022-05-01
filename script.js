260463dd4ae822bed875d129995183186e821291
ShubhamChachan <104708618+ShubhamChachan@users.noreply.github.com>
2022-05-01 10:57:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var seconds=document.getElementById('seconds');
+var milliseconds=document.getElementById('miliseconds');
+var start=document.getElementById('start');
+var stop=document.getElementById('stop');
+var reset=document.getElementById('reset');
+var sec=0;
+var millisec=0;
+
+
+
+const timer=()=>{
+    millisec++;  
+}
+console.log('timer', millisec)
